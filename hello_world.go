5847acdbc0effbde3b84d86a0041d1de42ebec71
Wei Yang <richard.weiyang@gmail.com>
2017-10-21 19:06:47 +0800

    send money from and to

--- hello_world.go
+++ hello_world.go
@@ -78,9 +78,80 @@ func (this *WalletsController) Get() {
 	this.Ctx.WriteString("</table></body></html>")
 }
 
+type WalletController struct {
+	beego.Controller
+}
+
+func (this *WalletController) Get() {
+	var lines []string
+	bc := NewBlockchain("3000")
+	defer bc.db.Close()
+
+	address := this.Ctx.Input.Param(":id")
+
+	this.Ctx.WriteString("<html><body>")
+	if ValidateAddress(address) {
+		lines = append(lines, fmt.Sprintf("Balance: %d", bc.getBalance(address)))
+		lines = append(lines, fmt.Sprintf("<form action=\"/wallet/%s\" method=\"post\">", address))
+		lines = append(lines, fmt.Sprintf("From:<br> <input type=\"text\" name=\"from\" value=\"%s\"> <br>", this.Ctx.Input.Param(":id")))
+		lines = append(lines, fmt.Sprintf("To:  <br> <input type=\"text\" name=\"to\"> <br>"))
+		lines = append(lines, fmt.Sprintf("Amount:  <br> <input type=\"text\" name=\"amount\"> <br><br>"))
+		lines = append(lines, fmt.Sprintf("<input type=\"submit\" value=\"Submit\"> </form>"))
+		this.Ctx.WriteString(strings.Join(lines, ""))
+	} else {
+		this.Ctx.WriteString("Invalid Address!")
+	}
+	this.Ctx.WriteString("</body></html>")
+}
+
+func (this *WalletController) Post() {
+	from := this.GetString("from")
+	to := this.GetString("to")
+	amount, _ := this.GetInt("amount")
+
+	this.Ctx.WriteString("<html><body>")
+
+	if !ValidateAddress(from) || !ValidateAddress(to) {
+		this.Ctx.WriteString("Check your Address")
+		return
+	}
+
+	bc := NewBlockchain("3000")
+	UTXOSet := UTXOSet{bc}
+	defer bc.db.Close()
+
+	if amount > bc.getBalance(from) {
+		this.Ctx.WriteString("You don't have enough money")
+		return
+	}
+
+	wallets, err := NewWallets("3000")
+	if err != nil {
+		this.Ctx.WriteString("Check your wallet")
+		return
+	}
+	wallet := wallets.GetWallet(from)
+	if wallet == nil {
+		fmt.Println()
+		this.Ctx.WriteString("The Address doesn't belongs to you!")
+		return
+	}
+
+	tx := NewUTXOTransaction(wallet, to, amount, &UTXOSet)
+	cbTx := NewCoinbaseTX(from, "")
+	txs := []*Transaction{cbTx, tx}
+
+	newBlock := bc.MineBlock(txs)
+	UTXOSet.Update(newBlock)
+
+	this.Ctx.WriteString("Succeed!</br></body></html>")
+	this.Ctx.WriteString("Go back to <a href=\"/\">Home</a> to see your block</body></html>")
+}
+
 func main() {
 	beego.Router("/", &MainController{})
 	beego.Router("/block/:id", &BlockController{})
 	beego.Router("/wallets", &WalletsController{})
+	beego.Router("/wallet/:id", &WalletController{})
 	beego.Run()
 }
