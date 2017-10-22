860d958c52f7d70de48dbe35d025c2dd6a152f58
Wei Yang <richard.weiyang@gmail.com>
2017-10-22 10:41:15 +0800

    use package blockchain_go

--- hello_world.go
+++ hello_world.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"github.com/astaxie/beego"
+	"github.com/richardweiyang/blockchain_go"
 )
 
 type MainController struct {
@@ -12,11 +13,11 @@ type MainController struct {
 }
 
 func (this *MainController) Get() {
-	bc := NewBlockchain("3000")
-	defer bc.db.Close()
+	chain := bc.NewBlockchain("3000")
+	defer chain.DB.Close()
 
 	this.Ctx.WriteString("<html>")
-	this.Ctx.WriteString(bc.PrintHTML())
+	this.Ctx.WriteString(chain.PrintHTML())
 	this.Ctx.WriteString("</html>")
 }
 
@@ -25,11 +26,11 @@ type BlockController struct {
 }
 
 func (this *BlockController) Get() {
-	var block *Block
-	bc := NewBlockchain("3000")
-	defer bc.db.Close()
+	var block *bc.Block
+	chain := bc.NewBlockchain("3000")
+	defer chain.DB.Close()
 
-	bci := bc.Iterator()
+	bci := chain.Iterator()
 
 	for {
 		block = bci.Next()
@@ -59,10 +60,10 @@ type WalletsController struct {
 }
 
 func (this *WalletsController) Get() {
-	wallets, err := NewWallets("3000")
+	wallets, err := bc.NewWallets("3000")
 	addresses := wallets.GetAddresses()
-	bc := NewBlockchain("3000")
-	defer bc.db.Close()
+	chain := bc.NewBlockchain("3000")
+	defer chain.DB.Close()
 
 	if err != nil {
 		this.Ctx.WriteString("<html><body>Invalid Wallet</body></html>")
@@ -72,7 +73,7 @@ func (this *WalletsController) Get() {
 	for _, address := range addresses {
 		var lines []string
 		lines = append(lines, fmt.Sprintf("<tr><td><a href=\"/wallet/%s\">%s</a></td>", address, address))
-		lines = append(lines, fmt.Sprintf("<td>%d</td></tr>", bc.getBalance(address)))
+		lines = append(lines, fmt.Sprintf("<td>%d</td></tr>", chain.GetBalance(address)))
 		this.Ctx.WriteString(strings.Join(lines, ""))
 	}
 	this.Ctx.WriteString("</table></body></html>")
@@ -84,14 +85,14 @@ type WalletController struct {
 
 func (this *WalletController) Get() {
 	var lines []string
-	bc := NewBlockchain("3000")
-	defer bc.db.Close()
+	chain := bc.NewBlockchain("3000")
+	defer chain.DB.Close()
 
 	address := this.Ctx.Input.Param(":id")
 
 	this.Ctx.WriteString("<html><body>")
-	if ValidateAddress(address) {
-		lines = append(lines, fmt.Sprintf("Balance: %d", bc.getBalance(address)))
+	if bc.ValidateAddress(address) {
+		lines = append(lines, fmt.Sprintf("Balance: %d", chain.GetBalance(address)))
 		lines = append(lines, fmt.Sprintf("<form action=\"/wallet/%s\" method=\"post\">", address))
 		lines = append(lines, fmt.Sprintf("From:<br> <input type=\"text\" name=\"from\" value=\"%s\"> <br>", this.Ctx.Input.Param(":id")))
 		lines = append(lines, fmt.Sprintf("To:  <br> <input type=\"text\" name=\"to\"> <br>"))
@@ -111,21 +112,21 @@ func (this *WalletController) Post() {
 
 	this.Ctx.WriteString("<html><body>")
 
-	if !ValidateAddress(from) || !ValidateAddress(to) {
+	if !bc.ValidateAddress(from) || !bc.ValidateAddress(to) {
 		this.Ctx.WriteString("Check your Address")
 		return
 	}
 
-	bc := NewBlockchain("3000")
-	UTXOSet := UTXOSet{bc}
-	defer bc.db.Close()
+	chain := bc.NewBlockchain("3000")
+	UTXOSet := bc.UTXOSet{chain}
+	defer chain.DB.Close()
 
-	if amount > bc.getBalance(from) {
+	if amount > chain.GetBalance(from) {
 		this.Ctx.WriteString("You don't have enough money")
 		return
 	}
 
-	wallets, err := NewWallets("3000")
+	wallets, err := bc.NewWallets("3000")
 	if err != nil {
 		this.Ctx.WriteString("Check your wallet")
 		return
@@ -137,11 +138,11 @@ func (this *WalletController) Post() {
 		return
 	}
 
-	tx := NewUTXOTransaction(wallet, to, amount, &UTXOSet)
-	cbTx := NewCoinbaseTX(from, "")
-	txs := []*Transaction{cbTx, tx}
+	tx := bc.NewUTXOTransaction(wallet, to, amount, &UTXOSet)
+	cbTx := bc.NewCoinbaseTX(from, "")
+	txs := []*bc.Transaction{cbTx, tx}
 
-	newBlock := bc.MineBlock(txs)
+	newBlock := chain.MineBlock(txs)
 	UTXOSet.Update(newBlock)
 
 	this.Ctx.WriteString("Succeed!</br></body></html>")
